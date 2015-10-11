cc0dc283bcdaa863963372dd70662460ee2b5827
Olga Samovolik <o.samovolik@datacentric.ru>
2015-10-11 17:30:59 +0300

    add database interaction

--- src/app.py
+++ src/app.py
@@ -1,18 +1,46 @@
 import random
-from flask import Flask, render_template
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
+import sqlite3
 
+# configuration
+DATABASE = '/tmp/flaskr.db'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 
 def get_excuse():
-    return unicode(random.choice(open("resources/excuses.txt").readlines()), "utf8")
+    cur = g.db.execute('select title from excuses order by random() limit 1')
+    return cur.fetchone()[0]
 
 
 @app.route("/")
 def index():
     return render_template("message.html", excuse=get_excuse())
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
 
 
 if __name__ == '__main__':
---
+++ src/resources/schema.sql
@@ -0,0 +1,14 @@
+drop table if exists excuses;
+create table excuses (
+  id integer primary key autoincrement,
+  title text not null
+);
+insert into excuses (id, title)
+  select 0 as id, "кампания уже заведена, но объемы появятся в понедельник" as title
+  union select 1, "просто эта стратегия слишком поздно запустилась"
+  union select 2, "у нас кластер нестабильный, все падает"
+  union select 3 ,"ну а что вы хотели на таком объеме для лукэлайка"
+  union select 4,"аналитик энжин упал"
+  union select 5,"в новом датацентре этой проблемы не будет"
+  union select 6,"мы снизим пороги, и мужчин станет больше. но это будут не совсем мужчины"
+  union select 7,"может, дойдет-таки auc хотя бы до 0.8";
