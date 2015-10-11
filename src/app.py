91db469c499d3c7ac074c8699a159c1eab103b4e
Veleslavia <okalistratova@gmail.com>
2015-10-11 16:39:51 +0300

    Add style

--- src/app.py
+++ src/app.py
@@ -1,12 +1,19 @@
 import random
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
+def get_excuse():
+    return unicode(random.choice(open("resources/excuses.txt").readlines()), "utf8")
+
+
 @app.route("/")
 def index():
-    return random.choice(open("resources/excuses.txt").readlines())
+    return render_template("message.html", excuse=get_excuse())
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
